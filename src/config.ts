bcf01cfb84da623b1fee92a9bfdec8265cf6f633
morgane <m.huebra@thecodingmachine.com>
2024-02-02 17:03:32 +0100

    [FEAT] updated config ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const rootLink = 'https://play.workadventu.re/_/r7g7sw8ndj/morganehuebratcm.github.io/workadventureBaseTemplate' // Here your map's url
+const rootLink = 'https://github.com/MorganeHuebraTCM/workadventureBaseTemplate' // Here your map's url
 const env: 'dev' | 'prod' =  "prod" // must be 'prod' on git
 
 export {
