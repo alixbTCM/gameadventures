962620b0da7d01a0b6972936b1ef85c7ab39672c
MorganeHuebraTCM <146930611+MorganeHuebraTCM@users.noreply.github.com>
2024-02-02 14:16:18 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const rootLink = 'http://localhost:5173' // Here your map's url
+const rootLink = 'https://play.workadventu.re/_/r7g7sw8ndj/morganehuebratcm.github.io/workadventureBaseTemplate' // Here your map's url
 const env: 'dev' | 'prod' =  "prod" // must be 'prod' on git
 
 export {
