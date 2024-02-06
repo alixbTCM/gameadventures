cea606c21be555db25bec9516105a103b1053437
MorganeHuebraTCM <146930611+MorganeHuebraTCM@users.noreply.github.com>
2024-02-06 08:44:20 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const rootLink = 'https://github.com/MorganeHuebraTCM/workadventureBaseTemplate' // Here your map's url
+const rootLink = 'https://morganehuebratcm.github.io/workadventureBaseTemplate/' // Here your map's url
 const env: 'dev' | 'prod' =  "prod" // must be 'prod' on git
 
 export {
